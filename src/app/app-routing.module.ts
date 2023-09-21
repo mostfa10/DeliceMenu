266ea5cdf4ec5fbf38be364998000013260f938c
mostfa <mostfalachheb20@gmail.com>
2023-09-21 17:22:26 +0200

    premier

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'menu', component: MenuComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
